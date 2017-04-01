a7fe4987895d6bbc950260cbf86063448c716190
chaluvadis <surendra.chaluvadi@gmail.com>
2017-04-01 23:10:11 +0400

    route config issue fixed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
+
 import App from './App/App';
 import './index.css';
 
-
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Route path="/" component={App} />
+  </Router>,
   document.getElementById('root')
 );
